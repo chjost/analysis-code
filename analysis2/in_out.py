364015a1d38bd8e0db3379681672b26c4982b168
Christopher Helmes <helmes@hiskp.uni-bonn.de>
2015-12-10 11:12:18 +0100

    improved stuff

--- analysis2/in_out.py
+++ analysis2/in_out.py
@@ -71,6 +71,57 @@ def read_vector(fname, column, skip, debug):
 
     return vector
 
+def inputnames(conf_file, corr_string):
+    """ Function to build Correlator input names conformal with B. Knippschilds
+    naming scheme
+        
+    The function invokes a config parser which reads the config file (config.ini) regarding the
+    specified correlation functions and returns a list of input names
+
+    Args:
+        conf_file: string with path to the configuration file 
+        corr_string: array of identifiers for correlation functions to be
+                    read in
+    Returns:
+        A list of inputnames
+
+    """
+    # invoke config parser and read config file
+    config = cp.SafeConfigParser()
+    config.read(conf_file)
+    # set section names for get operations, have to be the same in configfile
+    #quarks = config.get('quarks')
+    #operators = config.get('operator_lists')
+    #corr_list = config.get('correlator_lists')
+    # result list
+    inputnames = []
+    # Loop over correlation function names
+    for key in config.options('correlator_lists'):
+        # get value of each key splited by ':'
+        tmp = config.get('correlator_lists',key)
+        c0 = tmp.split(':')
+        # read only functions in corr_string, sort them into q and op arrays
+        if c0[0] in corr_string:
+            q_list = []
+            op_list = []
+            for val in c0[1:]:
+                if val[0] == 'Q':
+                    q_list.append(config.get('quarks',val))
+                elif val[0] == 'O':
+                    op_list.append(config.get('operator_lists',val))
+                else:
+                    print("Identifier not found")
+            # TODO: expand entrys in operator lists
+            # join single lists together for filename
+            join_q = ''.join(q_list)
+            join_op = '_'.join(op_list)
+            # build the filename
+            corrname = c0[0]+"/"+c0[0]+"_"+join_q+"_"+join_op+".dat"
+            print corrname
+            inputnames.append(corrname)
+    return inputnames
+  
+
 def read_matrix(fname, column, skip, debug):
     """Read a correlation function matrix from files.
 
--- corr_in.py
+++ corr_in.py
@@ -4,7 +4,7 @@
 import numpy as np
 import os
 import re
-import analysis as ana
+import analysis2 as ana
 
 ###############################################################################
 ############################### Start Main ####################################
@@ -19,37 +19,56 @@ def miss_confs(path,rng):
   return misslist
 
 def main():
-  T = 48
-  res_path='/hiskp2/helmes/contractions/kaon_scattering/B85.24/strange_186/data/'
-  #res_path='/hiskp2/helmes/contractions/pion_scattering/A40.24/data/'
-  Corrs = ana.inputnames('./charged.ini',['C2+', 'C4+C', 'C4+D'])
-  #Corrs = ana.inputnames('./charged.ini',['C2+'])
-  res = '/hiskp2/helmes/analysis/scattering/k_charged/data/B85.24/amu_s_186/' 
-  #res = '/hiskp2/helmes/analysis/scattering/pion_test/data/A40.24/' 
-  inputlist = []
-  cfg_rng = [500,2900,8]
-  missing = miss_confs(res_path,cfg_rng)
-  for i in range(cfg_rng[0],cfg_rng[1],cfg_rng[2]):
-    if i in missing:
-      continue
-    inputlist.append('cnfg%d/' % i)
-  # Read in correlators
-  print("Reading Correlation functions from %s..." % res_path)
-  print("C2")
-  C2 = ana.read_confs(res_path,Corrs[0],inputlist,T)
-  print("C4")
-  C4D = ana.read_confs(res_path,Corrs[1],inputlist,T)
-  C4C = ana.read_confs(res_path,Corrs[2],inputlist,T)
-  print("Read in done")
-  #subtract crossed from direct diagram
-  C4_tot = ana.confs_subtr(C4D,C4C)
-  C4_tot = ana.confs_mult(C4_tot,2)
-  print("Writing to: %s..." % res)
-  ana.write_data_ascii(C2,res+'k_charged_p0.dat')
-  #ana.write_data_ascii(C4C,res+'C4C_p0.0714.dat')
-  #ana.write_data_ascii(C4D,res+'C4D_p0.0714.dat')
-  ana.write_data_ascii(C4_tot,res+'kk_charged_A1_TP0_00.dat')
-  print("Finished")
+
+    # parse the input file
+    if len(sys.argv) < 2:
+        ens = ana.LatticeEnsemble.parse("charged.ini")
+    else:
+        ens = ana.LatticeEnsemble.parse(sys.argv[1])
+
+    # get data from input file
+    lat = ens.name
+    T = ens.get("T")
+    quark = ens.get_data("quark")
+    rawdir = ens.get_data("rawdir") + "/" + quark
+    datadir = ens.get_data("datadir") + "/" + quark
+    # TODO: Place that in the ensemble class
+    if len(sys.argv) < 2:
+      Corrs = ana.inputnames('charged.ini',['C2+', 'C4+C', 'C4+D'])
+    else:
+      Corrs = ana.inputnames(sys.argv[1],['C2+', 'C4+C', 'C4+D'])
+
+    print(rawdir)
+    print(datadir)
+    ## path to correlation functions
+    #res_path = '/hiskp2/helmes/contractions/kaon_scattering/tests/s_3_rnd_vec/A40.24/strange_225/data/'
+    ##res_path='/hiskp2/helmes/contractions/pion_scattering/A40.24/data/'
+    ##Corrs = ana.inputnames('./charged.ini',['C2+'])
+    ## path to analysis data
+    #res ='/hiskp2/helmes/analysis/scattering/test/s_3_rnd_vec/k_charged/data/A40.24/amu_s_225/'
+    ##res = '/hiskp2/helmes/analysis/scattering/pion_test/data/A40.24/' 
+    inputlist = []
+    cfg_rng = [714,2750,4]
+    missing = miss_confs(rawdir,cfg_rng)
+    for i in range(cfg_rng[0],cfg_rng[1],cfg_rng[2]):
+      if i in missing:
+        continue
+      inputlist.append('cnfg%d/' % i)
+    # Read in correlators
+    print("Reading Correlation functions from %s..." % rawdir)
+    print("C2")
+    C2 = ana.read_confs(rawdir,Corrs[0],inputlist,T)
+    print("C4")
+    C4D = ana.read_confs(rawdir,Corrs[1],inputlist,T)
+    C4C = ana.read_confs(rawdir,Corrs[2],inputlist,T)
+    print("Read in done")
+    #subtract crossed from direct diagram
+    C4_tot = ana.confs_subtr(C4D,C4C)
+    C4_tot = ana.confs_mult(C4_tot,2)
+    print("Writing to: %s..." % datadir)
+    ana.write_data_ascii(C2,datadir+'k_charged_p0.dat')
+    ana.write_data_ascii(C4_tot,datadir+'kk_charged_A1_TP0_00.dat')
+    print("Finished")
 
 
   
