59954012e972692f7c7cebe10da7286c8f32cc51
Christopher Helmes <helmes@hiskp.uni-bonn.de>
2015-12-14 08:37:18 +0100

    only input changed

--- corr_in.py
+++ corr_in.py
@@ -20,14 +20,14 @@ def miss_confs(path,rng):
 
 def main():
   T = 64
-  res_path='/hiskp2/helmes/contractions/kaon_scattering/A30.32/strange_2464/data/'
+  res_path='/hiskp2/helmes/contractions/kaon_scattering/tests/kaon_mass/A30.32/strange_225/s_3_rnd_vec/data/'
   #res_path='/hiskp2/helmes/contractions/pion_scattering/A40.24/data/'
   Corrs = ana.inputnames('./charged.ini',['C2+', 'C4+C', 'C4+D'])
   #Corrs = ana.inputnames('./charged.ini',['C2+'])
-  res = '/hiskp2/helmes/analysis/scattering/k_charged/data/A30.32/amu_s_2464/' 
+  res ='/hiskp2/helmes/analysis/mass/kaon/data/A30.32/s_3_rnd_vec/amu_s_225'
   #res = '/hiskp2/helmes/analysis/scattering/pion_test/data/A40.24/' 
   inputlist = []
-  cfg_rng = [1000,3241,8]
+  cfg_rng = [1000,1784,8]
   missing = miss_confs(res_path,cfg_rng)
   for i in range(cfg_rng[0],cfg_rng[1],cfg_rng[2]):
     if i in missing:
@@ -38,18 +38,18 @@ def main():
       (cfg_rng[0],cfg_rng[1],res_path))
   print("C2")
   C2 = ana.read_confs(res_path,Corrs[0],inputlist,T,verb=True)
-  print("C4")
-  C4D = ana.read_confs(res_path,Corrs[1],inputlist,T)
-  C4C = ana.read_confs(res_path,Corrs[2],inputlist,T)
+  #print("C4")
+  #C4D = ana.read_confs(res_path,Corrs[1],inputlist,T)
+  #C4C = ana.read_confs(res_path,Corrs[2],inputlist,T)
   print("Read in done")
   #subtract crossed from direct diagram
-  C4_tot = ana.confs_subtr(C4D,C4C)
-  C4_tot = ana.confs_mult(C4_tot,2)
+  #C4_tot = ana.confs_subtr(C4D,C4C)
+  #C4_tot = ana.confs_mult(C4_tot,2)
   print("Writing to: %s..." % res)
   ana.write_data_ascii(C2,res+'k_charged_p0.dat')
   #ana.write_data_ascii(C4C,res+'C4C_p0.0714.dat')
   #ana.write_data_ascii(C4D,res+'C4D_p0.0714.dat')
-  ana.write_data_ascii(C4_tot,res+'kk_charged_A1_TP0_00.dat')
+  #ana.write_data_ascii(C4_tot,res+'kk_charged_A1_TP0_00.dat')
   print("Finished")
 
 
