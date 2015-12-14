f94a9a56a7e9dc3ace795143e3164fcc8eb5e42d
Christopher Helmes <helmes@hiskp.uni-bonn.de>
2015-12-14 08:30:45 +0100

    tried to change corr_in.py to analysis2

--- analysis2/__init__.py
+++ analysis2/__init__.py
@@ -1,7 +1,7 @@
 """
 analysis package for scattering problems on the lattice
 """
-
+from .in_out import inputnames
 from .correlator import Correlators
 from .ensemble import LatticeEnsemble
 from .fit import LatticeFit, FitResult
--- corr_in.py
+++ corr_in.py
@@ -1,6 +1,6 @@
 #!/hadron/knippsch/Enthought/Canopy_64bit/User/bin/python
 
-
+import sys
 import numpy as np
 import os
 import re
@@ -27,11 +27,10 @@ def main():
         ens = ana.LatticeEnsemble.parse(sys.argv[1])
 
     # get data from input file
-    lat = ens.name
-    T = ens.get("T")
-    quark = ens.get_data("quark")
-    rawdir = ens.get_data("rawdir") + "/" + quark
-    datadir = ens.get_data("datadir") + "/" + quark
+    lat = ens.get_data("name")
+    T = ens.get_data("T")
+    rawdir = ens.get_data("rawdir") 
+    datadir = ens.get_data("datadir") 
     # TODO: Place that in the ensemble class
     if len(sys.argv) < 2:
       Corrs = ana.inputnames('charged.ini',['C2+', 'C4+C', 'C4+D'])
@@ -48,7 +47,7 @@ def main():
     #res ='/hiskp2/helmes/analysis/scattering/test/s_3_rnd_vec/k_charged/data/A40.24/amu_s_225/'
     ##res = '/hiskp2/helmes/analysis/scattering/pion_test/data/A40.24/' 
     inputlist = []
-    cfg_rng = [714,2750,4]
+    cfg_rng = [1000,1784,8]
     missing = miss_confs(rawdir,cfg_rng)
     for i in range(cfg_rng[0],cfg_rng[1],cfg_rng[2]):
       if i in missing:
@@ -58,16 +57,16 @@ def main():
     print("Reading Correlation functions from %s..." % rawdir)
     print("C2")
     C2 = ana.read_confs(rawdir,Corrs[0],inputlist,T)
-    print("C4")
-    C4D = ana.read_confs(rawdir,Corrs[1],inputlist,T)
-    C4C = ana.read_confs(rawdir,Corrs[2],inputlist,T)
+    #print("C4")
+    #C4D = ana.read_confs(rawdir,Corrs[1],inputlist,T)
+    #C4C = ana.read_confs(rawdir,Corrs[2],inputlist,T)
     print("Read in done")
     #subtract crossed from direct diagram
-    C4_tot = ana.confs_subtr(C4D,C4C)
-    C4_tot = ana.confs_mult(C4_tot,2)
+    #C4_tot = ana.confs_subtr(C4D,C4C)
+    #C4_tot = ana.confs_mult(C4_tot,2)
     print("Writing to: %s..." % datadir)
     ana.write_data_ascii(C2,datadir+'k_charged_p0.dat')
-    ana.write_data_ascii(C4_tot,datadir+'kk_charged_A1_TP0_00.dat')
+    #ana.write_data_ascii(C4_tot,datadir+'kk_charged_A1_TP0_00.dat')
     print("Finished")
 
 
