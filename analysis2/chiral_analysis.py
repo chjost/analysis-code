- fixed error in NLO chipt formula - nlo plot still looks fishy, does not coincide with computed physical point value

analysis2/chiral_analysis.py
@@ -1466,7 +1466,7 @@ class ChirAna(object):
       _chi = np.dot(cov,_res)
       return _chi
  
-  def fit_mu_a32(self,LO=False,xcut=None,debug=2):
+  def fit_mu_a32(self,plotdir=None,LO=False,xcut=None,debug=2):
       """ Fit the NLO chiPT formula to the data of self
       """
 
@@ -1484,6 +1484,10 @@ class ChirAna(object):
       _y = chut.concatenate_data(self.y_data)
       print(_x.shape)
       print(_y.shape)
+      #plot correlation of yvalues
+      corrmat = ana.LatticePlot(plotdir+"/corr_mat_nlo.pdf")
+      corrmat.plot_correlation(_y,["mu_a32_correlation","mu_a32"])
+      del corrmat
       # The data for the fit should be just two arrays containing the
       # bootstrapsamples
       #if debug > 3:

analysis2/chiral_functions.py
@@ -40,7 +40,8 @@ def calc_x_plot(x):
 def pik_I32_chipt_plot(args, x):
     """ Wrapper for plotfunction"""
     _x = x.reshape((len(x),1))
-    return pik_I32_chipt_nlo(_x[0],_x[1],_x[2], args[0,3], args[0,0:3])
+    #return pik_I32_chipt_nlo(_x[0],_x[1],_x[2], args[0,3], args[0,0:3])
+    return pik_I32_chipt_nlo(_x[0],_x[1],_x[2], args[0,-1], args[0,0:3])
 
 def pik_I32_chipt_lo_plot(args, x):
     """ Wrapper for plotfunction"""
@@ -64,16 +65,18 @@ def pik_I32_chipt_nlo(mpi, mk, fpi, r0, p, lambda_x=None):
     -------
     _mua32 : 1d-array, the calculated values of _mua32
     """
+    #check inputs
     if lambda_x is None:
         lambda_x = fpi
+    _p=p
     # Term with L_piK, a collection of Lower LECs
-    _sum1 = p[0]*32.*mpi*mk/fpi**2
+    _sum1 = _p[0]*32.*mpi*mk/fpi**2
     # Term with L5
-    _sum2 = p[1]*16.*mpi**2/fpi**2
+    _sum2 = _p[1]*16.*mpi**2/fpi**2
     # Term with NLO function (does not take eta mass at the moment)
-    _sum3 = chi_I32_nlo(lambda_x, mpi, mk)
+    _sum3 = chi_I32_nlo(lambda_x, mpi, mk)/(16.*np.pi**2*fpi**2)
     _mua32 = (reduced_mass(mpi,mk)/fpi)**2/(4.*np.pi)*(-1.+_sum1-_sum2+_sum3)
-    #+p[2]/(r0**2)
+    #+_p[2]/(r0**2)
     return _mua32
 
 def pik_I32_chipt_lo(mpi, mk, fpi, r0, p):
@@ -189,6 +192,5 @@ def mua0_I12_from_fit(pars,x):
 
 def mua0_I32_nlo_from_fit(pars,x):
     _x = np.atleast_2d(x)
-    print(_x)
     _mua0 = pik_I32_chipt_nlo(_x[:,1], _x[:,2], _x[:,3], _x[:,4], pars, lambda_x=None) 
     return _mua0

analysis2/chiral_utils.py
@@ -77,7 +77,7 @@ def evaluate_phys(x, args, args_w ,func, isdependend):
   needed = np.zeros((nsam,))
   result = np.full(nsam,np.nan)
   # loop over samples
-  print x.shape
+  print args[0], x[0]
   for b in range(nsam):
     y = func(args[b],x[b])
     if y.shape[0] == 2:

analysis2/fit.py
@@ -1043,7 +1043,12 @@ class FitResult(object):
         _mua0_phys = FitResult("mua0_phys",True)
         _shape = (_nsam,1)
         _mua0_phys.create_empty(_shape, _shape, len(self.data))
-        for res in evaluate_phys(x, _pars, _pars_w, func,False):
+        print("physical x-values:")
+        print(np.asarray((x[0,0],x[0,1],x[0,2],x[0,3],x[0,4])))
+        print("parameters")
+        print(_pars[0])
+        print(func(_pars[0],np.asarray((x[0,0],x[0,1],x[0,2],x[0,3],x[0,4]))))
+        for res in evaluate_phys(x, _pars, _pars_w, func, False):
             _mua0_phys.add_data(*res)
         return _mua0_phys
 