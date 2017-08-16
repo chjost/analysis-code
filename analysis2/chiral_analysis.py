using NLO chipt formula with fixed strange quark mass and finite size corrections for kaon, fpi and pion

analysis2/chiral_analysis.py
@@ -1477,12 +1477,15 @@ class ChirAna(object):
           print(tmp)
       elif interval >= 0.:
           tmp = x[:,0] < interval
+          print("bools for cut:")
+          print(tmp.shape)
       elif interval < 0.:
           tmp = x[:,0] > -interval
       print("y-shape before cut:")
       print(y.shape)
-      _x = x[tmp,...]
-      _y = y[tmp,...]
+      print(x.shape)
+      _x = x[tmp]
+      _y = y[tmp]
       print("y-data after cut:")
       print(_y[:,0])
       print("y-shape after cut:")
@@ -1561,7 +1564,10 @@ class ChirAna(object):
       if ren is None:
           ren = fpi
       if self.gamma is True:
-          _x = np.column_stack((ren,mpi,mk,fpi,meta))
+          if meta is not None:
+              _x = np.column_stack((ren,mpi,mk,fpi,meta))
+          else:
+              _x = np.column_stack((ren,mpi,mk,fpi))
           if iso_32 is True:
               self.phys_point_fitres = self.fitres.calc_mua0_pik_phys(_x,
                                                                   mua0_I32_from_fit)

analysis2/chiral_functions.py
@@ -174,13 +174,13 @@ def chi_nlo_pos(ren,mpi,mk,meta=None):
     return 1./(16.*np.pi**2) *(np.sum(_chi,axis=0)+43./9.)
 
 
-def gamma_pik(mpi, mk, mu_a0, fpi, ren=None):
+def gamma_pik(mpi, mk, mu_a0, fpi, meta=None, ren=None):
     if ren is None:
         ren = fpi 
     _res = np.zeros((3,mpi.shape[-1]))
     _res[0] = 4.*np.pi*fpi**2/reduced_mass(mpi,mk)**2*mu_a0
-    _res[1] = 1.+chi_nlo_neg(ren,mpi,mk,fpi)
-    _res[2] = -2.*mk*mpi/fpi**2*chi_nlo_pos(ren,mpi,mk)
+    _res[1] = 1.+chi_nlo_neg(ren,mpi,mk,fpi,meta)
+    _res[2] = -2.*mk*mpi/fpi**2*chi_nlo_pos(ren,mpi,mk,meta)
     _sum = np.sum(_res,axis=0)
     _gamma = -fpi**2/(16.*mpi**2)*_sum 
     return _gamma