b60be4e280130fb9e2dad5c72c481c225f415c96
chelmes <helmes@hiskp.uni-bonn.de>
2018-05-18 15:08:52 +0200

    Implement correlation plot

--- chiral_analysis_scripts/pik_I32_B_plot_correlation.py
+++ chiral_analysis_scripts/pik_I32_B_plot_correlation.py
@@ -24,7 +24,6 @@
 import sys
 from scipy import stats
 from scipy import interpolate as ip
-from scipy import optimize as opt
 import numpy as np
 from numpy.polynomial import polynomial as P
 import math
@@ -151,7 +150,6 @@ def get_priors(dataframe,obs,ordering,bname):
         _p = pivot_dataframe(dataframe,o,ordering,bname)
         _priors.append(_p)
     return pd.concat(_priors,axis=1)
-
 def mute(cov):
     _cov = np.zeros_like(cov)
     for i in range((_cov.shape[0]-2)/3):
@@ -159,6 +157,7 @@ def mute(cov):
     for i in range(_cov.shape[0]-2,_cov.shape[0]):
       _cov[i,i]=cov[i,i]
     return _cov
+
 def main():
 ################################################################################
 #                   set up objects                                             #
@@ -171,108 +170,45 @@ def main():
 
     # get data from input file
     #TODO: Could we hide that in a function?
-    #print(ens)
-    #lat = ens.name()
-    #space=ens.get_data("beta")
-    #latA = ens.get_data("namea")
-    #latB = ens.get_data("nameb")
-    #latD = ens.get_data("named")
-    #strangeA = ens.get_data("strangea")
-    #strangeB = ens.get_data("strangeb")
-    #strangeD = ens.get_data("stranged")
-    #strange_eta_A = ens.get_data("strange_alt_a")
-    #strange_eta_B = ens.get_data("strange_alt_b")
-    #strange_eta_D = ens.get_data("strange_alt_d")
     zp_meth=ens.get_data("zp_meth")
-    #external_seeds=ens.get_data("external_seeds_a")
-    #continuum_seeds=ens.get_data("continuum_seeds_b")
-    #amulA = ens.get_data("amu_l_a")
-    #amulB = ens.get_data("amu_l_b")
-    #amulD = ens.get_data("amu_l_d")
-    ##dictionary of strange quark masses
-    #amusA = ens.get_data("amu_s_a")
-    #amusB = ens.get_data("amu_s_b")
-    #amusD = ens.get_data("amu_s_d")
-    ## dictionaries for chiral analysis
-    #lat_dict = ana.make_dict(space,[latA,latB,latD])
-    #amu_l_dict = ana.make_dict(space,[amulA,amulB,amulD])
-    #mu_s_dict = ana.make_dict(space,[strangeA,strangeB,strangeD])
-    #mu_s_eta_dict = ana.make_dict(space,[strange_eta_A,strange_eta_B,strange_eta_D])
-    #amu_s_dict = ana.make_dict(space,[amusA,amusB,amusD])
-    #print(amu_s_dict)
     datadir = ens.get_data("datadir") 
+    plotdir = ens.get_data("plotdir") 
     resdir = ens.get_data("resultdir") 
-    # Load the data from the resultdir
+    # Load theata from the resultdir
     proc_id = 'piK_I32_unfixed_data_B%d'%(zp_meth) 
     unfixed_data_path = resdir+'/'+proc_id+'.h5' 
     unfixed_data = pd.read_hdf(unfixed_data_path,key=proc_id)
     unfixed_data.info()
     unfixed_A = unfixed_data.where(unfixed_data['beta']==1.90).dropna()
     unfixed_A.info()
-    xdata_Ab = unfixed_A[unfixed_A.nboot==0]
-    xdata_A = xdata_Ab.set_index(['L','mu_l','mu_s'],drop=False)[['mu_l','mu_s']].sort_index()
-    #xdata_A = xdata_A[xdata_A.index.get_level_values('nboot') == 0]
     A_priors = get_priors(unfixed_A,['r_0','Z_P'],['beta'],'nboot')
     A_data = get_dataframe_fit(unfixed_A,'M_K^2_FSE',                       
                             ['L','mu_l','mu_s'],'nboot',priors=A_priors)
-    A_data.info()
     data_for_cov = get_dataframe_fit(unfixed_A,'M_K^2_FSE',
                                    ['beta','L','mu_l','mu_s'],'nboot',priors=A_priors)
+    xdata_Ab = unfixed_A[unfixed_A.nboot==0]
+    xdata_A = xdata_Ab.set_index(['L','mu_l','mu_s'],drop=False)[['mu_l','mu_s']].sort_index()
+    print(xdata_A.values)
     data_for_cov.info()
     cov = get_covariance(data_for_cov)
     cov = mute(cov)
-    cov_iu = np.linalg.cholesky(np.linalg.inv(cov))
-    print(cov_iu)
-    print(A_data.iloc[0])
-    print(xdata_A)
-    def amk_sq(p,x):
-        # Parameters are equal for each beta, input data is not
-        return p[0]/(p[4]*p[3])*(x[:,0]+x[:,1])*(1+p[1]/p[3]*x[:,0]+p[2]/p[3]**2)
-    def errfunc_ms(p,x,y,cov_iu):
-        # cov_u is the upper triangular matrix of cov
-        f_vector = np.r_[amk_sq(p,x),p[3:5]]
-        return np.dot(cov_iu,y-f_vector)
-    p = np.array((5.558,0.143,4.845,5.193,0.524))
-    chi_vec_A = (errfunc_ms(p,xdata_A.values,A_data.iloc[0],cov_iu))
-    print(np.square(chi_vec_A))
-    print(np.nansum(np.square(chi_vec_A)))
+    print(cov)
+    print(np.linalg.eigvals(cov[0:3,0:3]))
+    corrcoef = np.corrcoef(data_for_cov.values.T)
+    # plot correlation coefficients in heatmap
+    tickmarks = np.r_[xdata_A.values,np.asarray([[0,3],[0,4]])]
+    with PdfPages(plotdir+'/correlation_fixms_beta19.pdf') as pdf:
+        if corrcoef.shape[0] != corrcoef.shape[1]:
+          raise ValueError("data not symmetric")
+        plt.figure(figsize=(13,12))
+        plt.xticks(np.arange(0.5,corrcoef.shape[0]+0.5),tickmarks,rotation=90)
+        plt.yticks(np.arange(0.5,corrcoef.shape[0]+0.5),tickmarks)
+        plt.pcolor(np.corrcoef(corrcoef), cmap=matplotlib.cm.bwr,
+                vmin=np.amin(corrcoef), vmax=np.amax(corrcoef))
+        plt.colorbar()
+        pdf.savefig()
+        plt.close()
 
-    # do a fit to the original data
-    amk_sq_fitresult = opt.least_squares(errfunc_ms,p, args=(xdata_A.values,
-                                         A_data.iloc[0], cov_iu))
-    print(amk_sq_fitresult.x)
-    # leastsquares only gives half of the answer
-    print(2*amk_sq_fitresult.cost)
-    print(amk_sq_fitresult.fun)
-    print(np.sum(np.square(amk_sq_fitresult.fun)))
-    print(amk_sq_fitresult.status,amk_sq_fitresult.message)
-
-    
-    #print(unfixed_data.sample(n=20))
-    # Fits take place per bootstrapsample need an errorfunction and a function
-    # Get beta dependent priors r0 and Pz as dataframe
-    #r0 = pivot_dataframe(unfixed_data,'r_0',['beta'],'nboot')
-    #r0.info()
-    #zp = pivot_dataframe(unfixed_data,'Z_P',['beta'],'nboot')
-    #zp.info()
-    #priors = pd.concat([r0,zp],axis=1)
-    #priors.info()
-    ## We also need a covariance matrix that gets inverted
-    #data_for_cov = get_dataframe_fit(unfixed_data,'M_K^2_FSE',
-    #                               ['beta','L','mu_l','mu_s'],'nboot',priors=priors)
-    #data_for_cov.info()
-    #cov = get_covariance(data_for_cov)
-    #get_measurements(unfixed_data,'M_K^2_FSE',['beta','L','mu_l','mu_s'],'nboot',beta)
-    #print(data_for_cov.sample(n=20))
-    #obs_of_interest = ['M_K^2_FSE']
-    #result_means = chi.bootstrap_means(unfixed_data,['beta','L','mu_l','mu_s'],obs_of_interest)
-    #chi.print_si_format(result_means)
-    #leastsquares takes callable function and arguments plus start estimate
-    # One lattice spacing
-    #hdfstorer = pd.HDFStore(unfixed_data_path)
-    #hdfstorer['raw_data'] = unfixed_data
-    #hdfstorer['covariancematrix'] = cov_matrix
-    #hdfstorer['fitresults']
 if __name__ == "__main__":
     try:
         main()
