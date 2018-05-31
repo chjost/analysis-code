4e3ee2052c3cde29896a8d6ebce75b81df77b647
chelmes <helmes@hiskp.uni-bonn.de>
2018-05-31 17:30:05 +0200

    NLO chiral extrapolation in place
    Need storage of results, calculation of physical scattering length

--- chiral_analysis_scripts/pik_I32_nlo_chipt.py
+++ chiral_analysis_scripts/pik_I32_nlo_chipt.py
@@ -172,30 +172,57 @@ def mu_a32_errfunc(p,x,y,cov):
     #print("In NLO-Errfunc shape of x-values is:")
     #print(x.shape)
     # expect two priors
+    #print(y.shape[0],x.shape[0])
     if y.shape[0] > x.shape[0]:
         #_res = pik_I32_chipt_nlo(x[:,0],x[:,1],x[:,2],x[:,3],p)-y[:-2]
-        _res = ana.pik_I32_chipt_nlo(x[:,0],x[:,1],x[:,2],x[:,3],p,meta=x[:,4])-y[:-1]
+        _res = ana.pik_I32_chipt_nlo(x[:,0],x[:,1],x[:,2],p,meta=x[:,3])-y[:-1]
         #_res = np.r_[_res,p[1]-y[-2],p[2]-y[-1]]
         _res = np.r_[_res,p[1]-y[-1]]
     else: 
-      _res = ana.pik_I32_chipt_nlo(x[:,0],x[:,1],x[:,2],x[:,3],p,meta=x[:,4])-y
+      _res = ana.pik_I32_chipt_nlo(x[:,0],x[:,1],x[:,2],p,meta=x[:,3])-y
     # calculate the chi values weighted with inverse covariance matrix
     _chi = np.dot(cov,_res)
     return _chi
+
+def df_cols_to_list(dataframe,names):
+    l1 = list(dataframe[names[0]].values)
+    l2 = list(dataframe[names[1]].values)
+    l3 = list(dataframe[names[2]].values)
+    return l1,l2,l3 
+
+def cut_data(dataframe,obs,interval=None):
+    """Based on a given observable limit the dataframe to contain the values
+    inside the observables range
+
+    Based on the original data of the observable the dataframe gets filtered
+    accordingly.
+    """
+    if interval is not None:
+        # get the 0th samples of the dataframe
+        cut_decide = dataframe[['beta','L','mu_l',obs]].loc[dataframe['sample']==0]
+        # Obtain a list of values for beta,L and mu_l that we like to keep
+        choice = cut_decide[['beta','L','mu_l']].loc[(cut_decide[obs]>interval[0]) &
+                                                     (cut_decide[obs]<interval[1])]
+        # need lists
+        ix_b_lst, ix_mu_lst, ix_l_lst = df_cols_to_list(choice,['beta','mu_l','L'])
+        # Select the data
+        cut = dataframe.loc[dataframe.beta.isin(ix_b_lst) &
+                            dataframe.mu_l.isin(ix_mu_lst) & 
+                            dataframe.L.isin(ix_l_lst)]
+    else:
+        cut = dataframe
+    return cut
+
 def main():
     pd.set_option('display.width',1000)
-
-
-
-################################################################################
-#                   set up objects                                             #
-################################################################################
     # Get parameters from initfile
     if len(sys.argv) < 2:
         ens = ana.LatticeEnsemble.parse("A40.24.ini")
     else:
         ens = ana.LatticeEnsemble.parse(sys.argv[1])
-
+################################################################################
+#                   set up objects                                             #
+################################################################################
     # get data from input file
     ms_fixing = sys.argv[2]
     epik_meth = ens.get_data("epik_meth") 
@@ -210,62 +237,66 @@ def main():
     interpolated_data = pd.read_hdf(data_path, key='Interpolate_sigma_%s'%epik_meth)
     interpolated_data.info()
     print(chi.bootstrap_means(interpolated_data,['beta','L','mu_l'],['mu_piK_a32']))
-    # To fit a function we need x and y data and a covariance matrix
-    # get the xdata without any errors
-    xdata = interpolated_data[['M_pi','M_K^2','M_eta^2','mu_piK_a32']].where(interpolated_data['sample']==0).dropna()
-    print(xdata)
-    # get the ydata
-    ydata = get_dataframe_fit(interpolated_data,'mu_piK_a32',['beta','L','mu_l'],'sample')
-    ydata.info()
-    # get the priors and a covariance matrix
-    l5samples = ana.draw_gauss_distributed(5.41e-3,3e-5,(nboot,),origin=True)
-    idx = np.arange(nboot)
-    L5 = pd.DataFrame(data=l5samples, index=idx,columns=['L_5']) 
-    L5.info()
-    data_for_cov = get_dataframe_fit(interpolated_data,'mu_piK_a32',
-                                   ['beta','L','mu_l'],'sample',priors=L5)
-    data_for_cov.info()
-    cov = get_covariance(data_for_cov)
-    # Our fit is uncorrelated
-    cov = np.diag(np.diagonal(cov))
-    #print(np.dot(np.linalg.inv(cov),cov))
-    cov_iu = np.linalg.cholesky(np.linalg.inv(cov))
-    print(cov_iu.T)
-    ## Fitresults dataframe by beta
-    col_names = ['nboot','chi^2','L_piK','L_5']
-    fitres = pd.DataFrame(columns = col_names)
-    p = np.array((1.,0.1))
-    for b in np.arange(nboot):
-        _tmp_fitres = opt.least_squares(mu_a32_errfunc, p, args=(xdata.values,
-                                        ydata.iloc[b], cov_iu.T))
-        _chisq = 2*_tmp_fitres.cost
-        _tmp_pars = dict(zip(col_names[3:],_tmp_fitres.x))
-        _res_dict = {'beta':beta,'nboot':b,'chi^2':_chisq}
-        _res_dict.update(_tmp_pars)
-        _tmpdf = pd.DataFrame(data = _res_dict,index=[b])
-        fitres = fitres.append(_tmpdf)
-        if b%100 == 0:
-            print(_res_dict)
-    fitres.info()
-   
-    #
-    ## We need a plot of the difference between the data and the fitfunction
-    ## Calculate difference
-    ## Errorbar Plot
-    ##with PdfPages(plotdir+'/D30.48_mksq_fit.pdf') as pdf
-    ##    plt.xlabel()
-    ##    pdf.savefig()
-    ## Can be done by pandas
-    #print("Fit Result Summary:")
-    #means = chi.bootstrap_means(fitres,['beta',],['chi^2','P_0','P_1']) 
-    #chi.print_si_format(means)
-    
-    #leastsquares takes callable function and arguments plus start estimate
-    # One lattice spacing
-    #hdfstorer = pd.HDFStore(unfixed_data_path)
-    #hdfstorer['raw_data'] = unfixed_data
-    #hdfstorer['covariancematrix'] = cov_matrix
-    #hdfstorer['fitresults']
+    # A few of the data are squared, we need the unsquared data
+    extrapol_df = pd.DataFrame(index=interpolated_data.index,
+                               data= interpolated_data[['beta','L','mu_l','sample',
+                                                        'fpi','M_pi',
+                                                        'mu_piK_a32']])
+    extrapol_df['M_K'] = interpolated_data['M_K^2'].pow(1./2)
+    extrapol_df['M_eta'] = interpolated_data['M_eta^2'].pow(1./2)
+    extrapol_df['mu_piK/fpi'] = extrapol_df['M_K']*extrapol_df['M_pi']/(extrapol_df['M_pi']+extrapol_df['M_K'])/extrapol_df['fpi']
+    groups = ['beta','L','mu_l']
+    obs = ['fpi','M_pi','M_K','M_eta','mu_piK_a32','mu_piK/fpi']
+    means = chi.bootstrap_means(extrapol_df,groups,obs)
+    chi.print_si_format(means)
+    fit_ranges=[[0.,2.5],[0.,1.41],[0.,1.35]]
+    for fr in fit_ranges:
+        fit_df = cut_data(extrapol_df,'mu_piK/fpi',fr)
+        # To fit a function we need x and y data and a covariance matrix
+        # get the xdata without any errors
+        xdata = fit_df[['beta','L','mu_l','M_pi','M_K',
+                             'fpi','M_eta']].where(fit_df['sample']==0).dropna()
+        xdata = xdata.set_index(['beta','L','mu_l'],drop=True).sort_index()
+        # get the priors needed for y vector and  covariance matrix
+        l5samples = ana.draw_gauss_distributed(5.41e-3,3e-5,(nboot,),origin=True)
+        idx = np.arange(nboot)
+        L5 = pd.DataFrame(data=l5samples, index=idx,columns=['L_5']) 
+        L5.info()
+        # get the ydata
+        ydata = get_dataframe_fit(fit_df,'mu_piK_a32',['beta','L','mu_l'],
+                                  'sample',priors=L5)
+        ydata.info()
+        print(xdata)
+        print(ydata.sample(n=20))
+        chi.print_si_format(means)
+        data_for_cov = get_dataframe_fit(fit_df,'mu_piK_a32',
+                                       ['beta','L','mu_l'],'sample',priors=L5)
+        data_for_cov.info()
+        cov = get_covariance(data_for_cov)
+        # Our fit is uncorrelated
+        cov = np.diag(np.diagonal(cov))
+        chi.print_si_format(means)
+        cov_iu = np.linalg.cholesky(np.linalg.inv(cov))
+        ## Fitresults dataframe by beta
+        col_names = ['nboot','chi^2','L_piK','L_5']
+        fitres = pd.DataFrame(columns = col_names)
+        p = np.array((1.,0.1))
+        for b in np.arange(nboot):
+            _tmp_fitres = opt.least_squares(mu_a32_errfunc, p, args=(xdata.values,
+                                            ydata.iloc[b].values, cov_iu))
+            _chisq = 2*_tmp_fitres.cost
+            _tmp_pars = dict(zip(col_names[2:],_tmp_fitres.x))
+            _res_dict = {'fr_bgn':fr[0],'fr_end':fr[1],'sample':b,'chi^2':_chisq}
+            _res_dict.update(_tmp_pars)
+            _tmpdf = pd.DataFrame(data = _res_dict,index=[b])
+            fitres = fitres.append(_tmpdf)
+            #if b%100 == 0:
+            #    print(xdata.values)
+            #    print(ydata.iloc[b].values)
+            #    print(_res_dict)
+        fitres.info()
+        chi.print_si_format(chi.bootstrap_means(fitres,['fr_end'],['L_piK','L_5','chi^2']))
+        # Store Fit dataframe with parameters and fitrange
 if __name__ == "__main__":
     try:
         main()
