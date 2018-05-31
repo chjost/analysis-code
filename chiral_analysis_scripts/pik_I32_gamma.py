c331c127e914c584e2309c56fca755f468a46c94
chelmes <helmes@hiskp.uni-bonn.de>
2018-05-31 19:08:34 +0200

    x-uncertainties working, think about distribution to end

--- chiral_analysis_scripts/pik_I32_gamma.py
+++ chiral_analysis_scripts/pik_I32_gamma.py
@@ -163,13 +163,14 @@ def gamma_errfunc(p,x,y,cov):
     #print("In NLO-Errfunc shape of x-values is:")
     #print(x.shape)
     # expect two priors
+    #print(y.shape[0],x.shape[0])
     if y.shape[0] > x.shape[0]:
         #_res = pik_I32_chipt_nlo(x[:,0],x[:,1],x[:,2],x[:,3],p)-y[:-2]
-        _res = p[1]-2*p[0]*x[:,0]-y[:-1]
+        _res = np.r_[x,p[-1]-2*p[-2]*x]-y[:-1]
         #_res = np.r_[_res,p[1]-y[-2],p[2]-y[-1]]
-        _res = np.r_[_res,p[1]-y[-1]]
+        _res = np.r_[_res,p[-1]-y[-1]]
     else: 
-      _res = np.r_[p[1]-2*p[0]*x]-y
+      _res = np.r_[x,p[-1]-2*p[-2]*x]-y
     # calculate the chi values weighted with inverse covariance matrix
     _chi = np.dot(cov,_res)
     return _chi
@@ -253,21 +254,22 @@ def main():
         fit_df = cut_data(extrapol_df,'M_K/M_pi',fr)
         # To fit a function we need x and y data and a covariance matrix
         # get the xdata without any errors
-        xdata = fit_df[['beta','L','mu_l','M_K/M_pi']].where(fit_df['sample']==0).dropna()
-        xdata = xdata.set_index(['beta','L','mu_l'],drop=True).sort_index()
+        #xdata = fit_df[['beta','L','mu_l','M_K/M_pi']].where(fit_df['sample']==0).dropna()
+        #xdata = xdata.set_index(['beta','L','mu_l'],drop=True).sort_index()
+        xdata = get_dataframe_fit(fit_df,'M_K/M_pi',['beta','L','mu_l'],'sample')
         # get the priors needed for y vector and  covariance matrix
         l5samples = ana.draw_gauss_distributed(5.41e-3,3e-5,(nboot,),origin=True)
         idx = np.arange(nboot)
         L5 = pd.DataFrame(data=l5samples, index=idx,columns=['L_5']) 
         L5.info()
         # get the ydata
-        ydata = get_dataframe_fit(fit_df,'Gamma',['beta','L','mu_l'],
+        ydata = get_dataframe_fit_xy(fit_df,'M_K/M_pi','Gamma',['beta','L','mu_l'],
                                   'sample',priors=L5)
         ydata.info()
         print(xdata)
         print(ydata.sample(n=20))
         chi.print_si_format(means)
-        data_for_cov = get_dataframe_fit(fit_df,'Gamma',
+        data_for_cov = get_dataframe_fit_xy(fit_df,'M_K/M_pi','Gamma',
                                        ['beta','L','mu_l'],'sample',priors=L5)
         data_for_cov.info()
         cov = get_covariance(data_for_cov)
@@ -278,20 +280,21 @@ def main():
         ## Fitresults dataframe by beta
         col_names = ['sample','chi^2','L_piK','L_5']
         fitres = pd.DataFrame(columns = col_names)
-        p = np.array((1.,0.1))
+        xp = np.arange(xdata.shape[0])
+        p = np.r_[xp,(1.,0.1)]
         for b in np.arange(nboot):
-            _tmp_fitres = opt.least_squares(gamma_errfunc, p, args=(xdata.values,
-                                            ydata.iloc[b].values, cov_iu))
+            _tmp_fitres = opt.least_squares(gamma_errfunc, p,
+                    args=(xdata.iloc[b].values, ydata.iloc[b].values, cov_iu))
             _chisq = 2*_tmp_fitres.cost
-            _tmp_pars = dict(zip(col_names[2:],_tmp_fitres.x))
+            _tmp_pars = dict(zip(col_names[2:],_tmp_fitres.x[-2:]))
             _res_dict = {'fr_bgn':fr[0],'fr_end':fr[1],'sample':b,'chi^2':_chisq}
             _res_dict.update(_tmp_pars)
             _tmpdf = pd.DataFrame(data = _res_dict,index=[b])
             fitres = fitres.append(_tmpdf)
-            #if b%100 == 0:
-            #    print(xdata.values)
-            #    print(ydata.iloc[b].values)
-            #    print(_res_dict)
+            if b%100 == 0:
+                print(xdata.iloc[b].values)
+                print(ydata.iloc[b].values)
+                print(_res_dict)
         fitres.info()
         fit_df=fit_df.merge(fitres,on='sample')
         chi.print_si_format(chi.bootstrap_means(fit_df,['beta','L','mu_l'],
--- chiral_analysis_scripts/run_chpt.sh
+++ chiral_analysis_scripts/run_chpt.sh
@@ -9,10 +9,10 @@ for m in "${meth[@]}"; do
         for e_meth in ${epik[@]}; do
             infile="../ini/pi_K/I_32${e}/chiral_analysis_mua0_zp${z}.ini"
             sed  -i "s/.*epik_meth.*/epik_meth=${e_meth}/" ${infile}
-            ./pik_I32_nlo_chipt.py ${infile} ${m} \
-            > ./logfiles/pik_I32_nlo_chipt_${m}${z}_${e_meth}${e}.log
-            #./pik_I32_gamma ${infile} ${m} \
-            #> ./logfiles/pik_I32_gamma_${m}${z}_${e_meth}${e}.log
+            #./pik_I32_nlo_chipt.py ${infile} ${m} \
+            #> ./logfiles/pik_I32_nlo_chipt_${m}${z}_${e_meth}${e}.log
+            ./pik_I32_gamma.py ${infile} ${m} \
+            > ./logfiles/pik_I32_gamma_${m}${z}_${e_meth}${e}.log
         done
     done
 done
